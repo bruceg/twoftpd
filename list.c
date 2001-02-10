150c45421c404f4be70473fa4d0b294256872699
Bruce Guenter <bruce@untroubled.org>
2001-02-10 03:38:57 +0000

    Simplified the output list format.

--- list.c
+++ list.c
@@ -5,36 +5,34 @@
 #include <time.h>
 #include <unistd.h>
 #include "twoftpd.h"
-
-extern int do_chroot;
+#include "backend.h"
 
 static char* format_mode(int mode, char* buf)
 {
-  if (S_ISLNK(mode)) *buf = 'l';
-  else if (S_ISREG(mode)) *buf = '-';
-  else if (S_ISDIR(mode)) *buf = 'd';
-  else if (S_ISCHR(mode)) *buf = 'c';
-  else if (S_ISBLK(mode)) *buf = 'b';
-  else if (S_ISFIFO(mode)) *buf = 'p';
-  else if (S_ISSOCK(mode)) *buf = 's';
-  else *buf = '?';
-  buf++;
-  *buf++ = (mode & S_IRUSR) ? 'r' : '-';
-  *buf++ = (mode & S_IWUSR) ? 'w' : '-';
-  *buf++ = (mode & S_ISUID) ?
-    (mode & S_IXUSR) ? 's' : 'S' :
-    (mode & S_IXUSR) ? 'x' : '-';
-  *buf++ = (mode & S_IRGRP) ? 'r' : '-';
-  *buf++ = (mode & S_IWGRP) ? 'w' : '-';
-  *buf++ = (mode & S_ISGID) ?
-    (mode & S_IXGRP) ? 's' : 'S' :
-    (mode & S_IXGRP) ? 'x' : '-';
-  *buf++ = (mode & S_IROTH) ? 'r' : '-';
-  *buf++ = (mode & S_IWOTH) ? 'w' : '-';
-  *buf++ = (mode & S_ISVTX) ?
-    (mode & S_IXOTH) ? 't' : 'T' :
-    (mode & S_IXOTH) ? 'x' : '-';
-  return buf;
+  if (S_ISDIR(mode)) strcpy(buf, "drwxr-xr-x");
+  else if (S_ISREG(mode)) strcpy(buf, "-rw-r--r--");
+  else strcpy(buf, "??????????");
+  return buf + 10;
+}
+
+static char* format_owner(uid_t owner, char* buf)
+{
+  unsigned i;
+  if (owner == uid) {
+    if (user_len < 8)
+      memcpy(buf, user, user_len);
+    for (i = user_len; i < 8; i++)
+      buf[i] = SPACE;
+  }
+  else
+    memcpy(buf, "somebody", 8);
+  return buf + 8;
+}
+
+static char* format_group(gid_t group, char* buf)
+{
+  memcpy(buf, (group == gid) ? "group   " : "somegrp ", 8);
+  return buf + 8;
 }
 
 static char* format_num(unsigned num, unsigned digits, unsigned max,
@@ -57,40 +55,6 @@ static char* format_num(unsigned num, unsigned digits, unsigned max,
     }
     buf += digits;
   }
-  return buf;
-}
-
-static char* format_owner(uid_t uid, char* buf)
-{
-  static struct passwd* pw = 0;
-  unsigned i;
-  char* ptr;
-
-  if (!pw || pw->pw_uid != uid) pw = getpwuid(uid);
-  if (pw && pw->pw_name) {
-    for (i = 0, ptr = pw->pw_name; *ptr && i < 8; ++i)
-      *buf++ = *ptr++;
-    for (; i < 8; ++i) *buf++ = SPACE;
-  }
-  else
-    buf = format_num(uid, 8, 99999999, buf);
-  return buf;
-}
-
-static char* format_group(gid_t gid, char* buf)
-{
-  static struct group* gr = 0;
-  unsigned i;
-  char* ptr;
-
-  if (!gr || gr->gr_gid != gid) gr = getgrgid(gid);
-  if (gr && gr->gr_name) {
-    for (i = 0, ptr = gr->gr_name; *ptr && i < 8; ++i)
-      *buf++ = *ptr++;
-    for (; i < 8; ++i) *buf++ = SPACE;
-  }
-  else
-    buf = format_num(gid, 8, 99999999, buf);
   return buf;
 }
 
@@ -118,15 +82,9 @@ static void format_stat(const struct stat* s, const char* filename,
   start = buf;
   namelen = strlen(filename);
   buf = format_mode(s->st_mode, buf); *buf++ = SPACE;
-  buf = format_num(s->st_nlink, 4, 9999, buf); *buf++ = SPACE;
-  if (do_chroot) {
-    buf = format_num(s->st_uid, 8, 99999999, buf); *buf++ = SPACE;
-    buf = format_num(s->st_gid, 8, 99999999, buf); *buf++ = SPACE;
-  }
-  else {
-    buf = format_owner(s->st_uid, buf); *buf++ = SPACE;
-    buf = format_group(s->st_gid, buf); *buf++ = SPACE;
-  }
+  memcpy(buf, "    1 ", 6); buf += 6;
+  buf = format_owner(s->st_uid, buf); *buf++ = SPACE;
+  buf = format_group(s->st_gid, buf); *buf++ = SPACE;
   buf = format_num(s->st_size, 8, 99999999, buf); *buf++ = SPACE;
   buf = format_time(s->st_mtime, buf); *buf++ = SPACE;
 
