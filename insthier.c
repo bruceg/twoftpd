a67ee66e48fac05d65d808b2f179eb6f7c00601b
Bruce Guenter <bruce@untroubled.org>
2001-02-23 04:15:39 +0000

    Use new installer framework.

--- insthier.c
+++ insthier.c
@@ -1,12 +1,20 @@
+#include "conf_bin.c"
+#include "conf_man.c"
 #include "installer.h"
 
 void insthier(void)
 {
+  int bin;
+  int man;
+  
+  bin = opendir(conf_bin);
+  man = opendir(conf_man);
+  
   c(bin, 0,      "twoftpd-anon",   -1, -1, 0755);
   c(bin, 0,      "twoftpd-auth",   -1, -1, 0755);
   c(bin, 0,      "twoftpd-xfer",   -1, -1, 0755);
 
   d(man,         "man1",           -1, -1, 0755);
-  c(man, "man1", "twoftpd-auth.1", -1, -1, 0755);
-  c(man, "man1", "twoftpd-xfer.1", -1, -1, 0755);
+  c(man, "man1", "twoftpd-auth.1", -1, -1, 0644);
+  c(man, "man1", "twoftpd-xfer.1", -1, -1, 0644);
 }
