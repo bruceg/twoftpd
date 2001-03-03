9ba3b32fed9a318c6467ac440d791373a0fd3354
Bruce Guenter <bruce@untroubled.org>
2001-03-03 01:30:30 +0000

    Modified to use timeout_write.

--- retr.c
+++ retr.c
@@ -21,49 +21,59 @@
 #include "twoftpd.h"
 #include "backend.h"
 
-static int copy(ibuf* in, obuf* out)
+static int copy(ibuf* in, int out)
 {
   char buf[iobuf_bufsize];
+  char obuf[iobuf_bufsize*2];
   char* ptr;
   char* prev;
+  char* optr;
   unsigned count;
+  unsigned ocount;
+  unsigned diff;
   
-  if (ibuf_eof(in)) return 1;
-  if (ibuf_error(in) || obuf_error(out)) return 0;
+  if (ibuf_error(in)) return 0;
   do {
     if (!ibuf_read(in, buf, sizeof buf) && in->count == 0) break;
     count = in->count;
-    prev = buf;
-    if (!binary_flag) {
-      for(;;) {
-	if ((ptr = memchr(prev, LF, count)) == 0) break;
-	if (!obuf_write(out, prev, ptr - prev)) return 0;
-	if (!obuf_write(out, "\r\n", 2)) return 0;
-	prev = ptr + 1;
-	count = buf + in->count - prev;
-      }
+    if (binary_flag) {
+      optr = buf;
+      ocount = count;
     }
-    if (!obuf_write(out, prev, count)) return 0;
+    else {
+      prev = buf;
+      optr = obuf;
+      ocount = 0;
+      for (;;) {
+	if ((ptr = memchr(prev, LF, count)) == 0) break;
+	diff = ptr - prev;
+	memcpy(optr, prev, diff); optr += diff; ocount += diff;
+	memcpy(optr, "\r\n", 2); optr += 2; ocount += 2;
+	prev = ptr + 1;
+	count -= diff + 1;
+      }
+      optr = obuf;
+    }
+    if (!timeout_write(out, optr, ocount)) return 0;
   } while (!ibuf_eof(in));
   if (!ibuf_eof(in)) return 0;
-  if (!obuf_flush(out)) return 0;
   return 1;
 }
       
 int handle_retr(void)
 {
   ibuf in;
-  obuf out;
+  int out;
   int result;
   
   if (!ibuf_open(&in, req_param, 0))
     return respond(550, 1, "Could not open input file.");
-  if (!make_out_connection(&out)) {
+  if ((out = make_connection()) == -1) {
     ibuf_close(&in);
     return 1;
   }
-  result = copy(&in, &out);
+  result = copy(&in, out);
   ibuf_close(&in);
-  obuf_close(&out);
+  close(out);
   return result ? respond(226, 1, "File sent successfully.") : 1;
 }
