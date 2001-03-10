dcd8a48590da7bc6518ce3828bd03e816eaf066c
Bruce Guenter <bruce@untroubled.org>
2001-03-10 02:23:59 +0000

    Use new "systime.h" magic include.

--- backend.c
+++ backend.c
@@ -16,7 +16,7 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 #include <stdlib.h>
-#include <time.h>
+#include "systime.h"
 #include <unistd.h>
 #include "twoftpd.h"
 #include "backend.h"
