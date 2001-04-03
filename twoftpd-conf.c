3062f8ae4385b65f45afaf49dad4e381b9a387a1
Bruce Guenter <bruce@untroubled.org>
2001-04-03 03:40:27 +0000

    Added a twoftpd-conf program.

---
+++ twoftpd-conf.c
@@ -0,0 +1,112 @@
+/* twoftpd-conf.c - Configure a new instance of twoftpd auth/xfer
+ * Copyright (C) 2001  Bruce Guenter <bruceg@em.ca>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+#include <pwd.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include "iobuf/iobuf.h"
+#include "err/err.h"
+
+const char program[] = "twoftpd-conf";
+static const char usage_str[] =
+"usage: twoftpd-conf logacct /twoftpd /twoftpd/log cvm [ip [chroot]]\n";
+
+void usage(const char* msg)
+{
+  if (msg)
+    obuf_put4s(&outbuf, program, ": ", msg, ".\n");
+  obuf_puts(&outbuf, usage_str);
+  obuf_flush(&outbuf);
+  exit(1);
+}
+
+static struct passwd* logacct;
+static const char* maindir;
+static const char* logdir;
+static const char* cvmpath;
+static const char* ip = "0.0.0.0";
+static const char* dochroot;
+
+static obuf out;
+
+int main(int argc, char* argv[])
+{
+  if (argc < 5) usage("Too few parameters");
+  if (argc > 7) usage("Too many parameters");
+  logacct = getpwnam(argv[1]);
+  maindir = argv[2];
+  logdir = argv[3];
+  if (maindir[0] != '/' || logdir[0] != '/')
+    die1(1, "Directory names must start with /.");
+  cvmpath = argv[4];
+  if (argc > 5) {
+    ip = argv[5];
+    if (argc > 6) dochroot = argv[6];
+  }
+  if (!logacct) die1(1, "Unknown logacct user name");
+
+  umask(0);
+
+  if (mkdir(maindir, 0755) == -1) die1sys(1, "Error creating main directory");
+  if (chmod(maindir, 01755) == -1)
+    die1sys(1, "Error setting permissions on main directory");
+  
+  if (mkdir(logdir, 0700) == -1) die1sys(1, "Error creating log directory");
+  if (chown(logdir, logacct->pw_uid, logacct->pw_gid) == -1)
+    die1sys(1, "Error setting owner on log directory");
+
+  if (chdir(maindir) == -1) die1sys(1, "Error changing to main directory");
+  if (mkdir("log", 0755) == -1)
+    die1sys(1, "Error creating log service directory");
+  if (mkdir("env", 0755) == -1)
+    die1sys(1, "Error creating env directory");
+
+  if (!obuf_open(&out, "run", OBUF_CREATE|OBUF_EXCLUSIVE, 0755, 0) ||
+      !obuf_put7s(&out,
+		  "#!/bin/sh\n"
+		  "exec 2>&1\n"
+		  "umask 022\n"
+		  "exec \\\n"
+		  "envdir ", maindir, "/env \\\n"
+		  "tcpserver -DRHv -llocalhost ", ip, " 21 \\\n"
+		  "softlimit -m 2000000 \\\n"
+		  "twoftpd-auth \\\n",
+		  cvmpath, " \\\n"
+		  "/usr/bin/twoftpd-xfer\n") ||
+      !obuf_close(&out))
+    die1sys(1, "Error writing run file");
+
+  if (!obuf_open(&out, "log/run", OBUF_CREATE|OBUF_EXCLUSIVE, 0755, 0) ||
+      !obuf_put5s(&out,
+		  "#!/bin/sh\n"
+		  "exec \\\n"
+		  "setuidgid ", logacct->pw_name, " \\\n"
+		  "multilog t ", logdir, "\n") ||
+      !obuf_close(&out))
+    die1sys(1, "Error creating log/run file");
+  
+  if (dochroot) {
+    if (!obuf_open(&out, "env/CHROOT", OBUF_CREATE|OBUF_EXCLUSIVE, 0644, 0) ||
+	!obuf_put2s(&out, dochroot, "\n") ||
+	!obuf_close(&out))
+      die1sys(1, "Error creating env/CHROOT file");
+  }
+  
+  return 0;
+}
