1b1d6ebf4abddc1bc6a7240d0bf0614dc1d9f6dd
Bruce Guenter <bruce@untroubled.org>
2001-04-05 11:21:57 +0000

    Added new executables.

--- insthier.c
+++ insthier.c
@@ -10,11 +10,13 @@ void insthier(void)
   bin = opendir(conf_bin);
   man = opendir(conf_man);
   
-  c(bin, 0,      "twoftpd-anon",   -1, -1, 0755);
-  c(bin, 0,      "twoftpd-auth",   -1, -1, 0755);
-  c(bin, 0,      "twoftpd-xfer",   -1, -1, 0755);
+  c(bin, 0,      "twoftpd-anon",      -1, -1, 0755);
+  c(bin, 0,      "twoftpd-anon-conf", -1, -1, 0755);
+  c(bin, 0,      "twoftpd-auth",      -1, -1, 0755);
+  c(bin, 0,      "twoftpd-conf",      -1, -1, 0755);
+  c(bin, 0,      "twoftpd-xfer",      -1, -1, 0755);
 
-  d(man,         "man1",           -1, -1, 0755);
-  c(man, "man1", "twoftpd-auth.1", -1, -1, 0644);
-  c(man, "man1", "twoftpd-xfer.1", -1, -1, 0644);
+  d(man,         "man1",              -1, -1, 0755);
+  c(man, "man1", "twoftpd-auth.1",    -1, -1, 0644);
+  c(man, "man1", "twoftpd-xfer.1",    -1, -1, 0644);
 }
