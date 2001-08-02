26b73055905a0fba105983e48d03586703000d79
Bruce Guenter <bruce@untroubled.org>
2001-08-02 09:42:32 +0000

    Added support for printing out the real file mode instead of a fabrication in long listings.

--- list.c
+++ list.c
@@ -29,10 +29,38 @@
 
 static obuf out;
 
+static const char* mode2str(int mode)
+{
+  static char buf[11];
+  buf[0] = S_ISREG(mode) ? '-' :
+    S_ISDIR(mode) ? 'd' :
+    S_ISCHR(mode) ? 'c' :
+    S_ISBLK(mode) ? 'b' :
+    S_ISFIFO(mode) ? 'p' :
+    S_ISSOCK(mode) ? 's' :
+    '?';
+  buf[1] = (mode & 0400) ? 'r' : '-';
+  buf[2] = (mode & 0200) ? 'w' : '-';
+  buf[3] = (mode & 04000) ?
+    (mode & 0100) ? 's' : 'S' :
+    (mode & 0100) ? 'x' : '-';
+  buf[4] = (mode & 0040) ? 'r' : '-';
+  buf[5] = (mode & 0020) ? 'w' : '-';
+  buf[6] = (mode & 02000) ?
+    (mode & 0010) ? 's' : 'S' :
+    (mode & 0010) ? 'x' : '-';
+  buf[7] = (mode & 0004) ? 'r' : '-';
+  buf[8] = (mode & 0002) ? 'w' : '-';
+  buf[9] = (mode & 01000) ?
+    (mode & 0001) ? 's' : 'S' :
+    (mode & 0001) ? 'x' : '-';
+  buf[10] = 0;
+  return buf;
+}
+
 static int output_mode(int mode)
 {
-  return obuf_write(&out, S_ISDIR(mode) ? "drwxr-xr-x" :
-		    S_ISREG(mode) ? "-rw-r--r--" : "??????????", 10);
+  return obuf_write(&out, mode2str(mode), 10);
 }
 
 static int output_owner(uid_t owner)
