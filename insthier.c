4bfba5e901a64fe8a3b6742f59f9407aeddf6919
Bruce Guenter <bruce@untroubled.org>
2001-08-16 03:21:21 +0000

    Adjusted to new insthier prototype.

--- insthier.c
+++ insthier.c
@@ -4,19 +4,16 @@
 
 void insthier(void)
 {
-  int bin;
-  int man;
+  int bin = opendir(conf_bin);
+  int man = opendir(conf_man);
+  int man1 = d(man, "man1", -1, -1, 0755);
   
-  bin = opendir(conf_bin);
-  man = opendir(conf_man);
-  
-  c(bin, 0,      "twoftpd-anon",      -1, -1, 0755);
-  c(bin, 0,      "twoftpd-anon-conf", -1, -1, 0755);
-  c(bin, 0,      "twoftpd-auth",      -1, -1, 0755);
-  c(bin, 0,      "twoftpd-conf",      -1, -1, 0755);
-  c(bin, 0,      "twoftpd-xfer",      -1, -1, 0755);
+  c(bin,  "twoftpd-anon",      -1, -1, 0755);
+  c(bin,  "twoftpd-anon-conf", -1, -1, 0755);
+  c(bin,  "twoftpd-auth",      -1, -1, 0755);
+  c(bin,  "twoftpd-conf",      -1, -1, 0755);
+  c(bin,  "twoftpd-xfer",      -1, -1, 0755);
 
-  d(man,         "man1",              -1, -1, 0755);
-  c(man, "man1", "twoftpd-auth.1",    -1, -1, 0644);
-  c(man, "man1", "twoftpd-xfer.1",    -1, -1, 0644);
+  c(man1, "twoftpd-auth.1",    -1, -1, 0644);
+  c(man1, "twoftpd-xfer.1",    -1, -1, 0644);
 }
